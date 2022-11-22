db779cfeca9dae1fc56a4fb3cf24408487cd2521
Nan Lin <linnan@cambricon.com>
2022-11-22 21:37:57 +0800

    add arg collector

---
+++ template_args_pack.cpp
@@ -0,0 +1,12 @@
+/*************************************************************************
+ * Copyright (C) [2020-2023] by Cambricon, Inc.
+ * Description: 
+ *************************************************************************/
+#include "./template_args_pack.hpp"
+
+int main() {
+  auto a = CollectArgs(1, 2, std::vector<int>({3,4,5}), 6, std::vector<int>({7, 8}));
+  for (auto i : a) {
+    std::cout << i;
+  }
+}
---
+++ template_args_pack.hpp
@@ -0,0 +1,36 @@
+/*************************************************************************
+ * Copyright (C) [2020-2023] by Cambricon, Inc.
+ * Description: 
+ *************************************************************************/
+#include <vector>
+#include <iostream>
+template<class T, class...R>
+std::vector<T> CollectArgs(const T &a, R... args);
+
+template<class T, class...R>
+std::vector<T> CollectArgs(const std::vector<T> &a, R... args);
+
+template<class T>
+std::vector<T> CollectArgs(const T &a) {
+  return {a};
+}
+
+template<class T>
+std::vector<T> CollectArgs(const std::vector<T> &a) {
+  return a;
+}
+
+template<class T, class...R>
+std::vector<T> CollectArgs(const T &a, R... args) {
+  auto ret = CollectArgs(args...);
+  ret.insert(ret.begin(), a);
+  return ret;
+}
+
+template<class T, class...R>
+std::vector<T> CollectArgs(const std::vector<T> &a, R... args) {
+  auto ret = CollectArgs(args...);
+  ret.insert(ret.begin(), a.begin(), a.end());
+  return ret;
+}
+
